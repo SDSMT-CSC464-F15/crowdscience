a76823dabf2471e04588cf3f479e6358f8c11e7e
hannahaker <hannah.aker@mines.sdsmt.edu>
2016-02-19 20:45:59 -0700

    testing getting data based on selection

--- crowdscience/eventsetselect.php
+++ crowdscience/eventsetselect.php
@@ -45,7 +45,7 @@ function updateEventSetData()
 $eventdata = $db->$eventset;
 $eventsetsinfo = $db->eventsetsinfo;
 
-$details = $eventsetsinfo->findOne( array('id' => '$eventset'), array('details') );
+$details = $eventsetsinfo->findOne( array('id' => '$eventset') );
 
 $response["details"] = $details;
 return $response;
