b1a9f82f355ed844ccaee0afc644ddce421ce6c9
hannahaker <hannah.aker@mines.sdsmt.edu>
2016-02-19 21:00:50 -0700

    testing getting data based on selection

--- crowdscience/eventsetselect.js
+++ crowdscience/eventsetselect.js
@@ -59,10 +59,12 @@ function updateEventSetSelect (data) {
 function updateEventSetData (data){
 $("#ev_table_head").empty();
 		ev_head_text = "<tr><th> User </th>";
+		
+		alert(data.details.length)
 		for (var i = data.details.length - 1; i >= 0; i--) {
-		ev_head_text+="<th>" + data.details[i]+ "</th>";
+		ev_head_text = ev_head_text + "<th>" + data.details[i]+ "</th>";
 		}
-		ev_head_text+="</tr>";
+		ev_head_text = ev_head_text + "</tr>";
 
 		$("#ev_table_head").append(ev_head_text);
 		
