a75caf88e98fcb8446cf5f24acb0aaa6582fd081
hannahaker <hannah.aker@mines.sdsmt.edu>
2016-02-28 22:57:30 -0700

    Adding view event page

--- crowdscience/viewEvent.js
+++ crowdscience/viewEvent.js
@@ -29,8 +29,10 @@ function DisplayEvent (data)
 {	
 	alert( "In DisplayEvent Function" );
 	$("#event_info").empty();
-	alert( "data.eventdata.location.coordinates : " + data.eventdata.location.coordinates[0] + ", " + data.eventdata.location.coordinates[1]);
-	eventInfo = "<b>User:</b><input class=\"event-control\" value=\"" + data.eventdata.user + "\" type=\"text\" disabled /> <b>Location:</b><input class=\"event-control\" value=\"" + data.eventdata.location.coordinates[0] + ", " + data.eventdata.location.coordinates[1] + "\" type=\"text\" disabled />" ;
+	alert( "data.eventdata.user : " + data.eventdata.user );
+	alert( "data.eventdata.location.coordinates : " + data.eventdata.location.coordinates );
+	
+	eventInfo = "<b>User:</b><input class=\"event-control\" value=\"" + data.eventdata.user + "\" type=\"text\" disabled /> <b>Location:</b><input class=\"event-control\" value=\"" + data.eventdata.location.coordinates + "\" type=\"text\" disabled />" ;
 	alert( "EventInfo okay");
 	for (var i = data.details.length - 1; i >= 0; i--) {
 		alert( "Data.details loop i : " + i );
